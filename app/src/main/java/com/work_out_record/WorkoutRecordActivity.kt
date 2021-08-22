b516bc57c9bfd4133445a9364780f5d84bc37980
kkcchh9999 <kkcchh9999@naver.com>
2021-08-22 22:40:52 +0900

    fragment 생성, recycler veiw 생성

---
+++ app/src/main/java/com/work_out_record/WorkoutRecordActivity.kt
@@ -0,0 +1,19 @@
+package com.work_out_record
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WorkoutRecordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_workout_record)
+
+        val isFragmentContainerEmpty = savedInstanceState == null
+        if (isFragmentContainerEmpty) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, WorkoutRecordFragment.newInstance())
+                .commit()
+        }
+    }
+}
