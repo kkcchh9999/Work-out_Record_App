9c2be412ff5824639b84f8c81205958c7e7830f5
권철현 <kkcchh9999@naver.com>
2021-09-09 22:17:02 +0900

    달력 개선

--- app/src/main/java/com/work_out_record/WorkoutCalendarFragment.kt
+++ app/src/main/java/com/work_out_record/WorkoutCalendarFragment.kt
@@ -8,12 +8,14 @@ import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.prolificinteractive.materialcalendarview.CalendarDay
 import com.prolificinteractive.materialcalendarview.DayViewDecorator
 import com.prolificinteractive.materialcalendarview.DayViewFacade
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -24,8 +26,11 @@ class WorkoutCalendarFragment : Fragment() {
     private lateinit var calendarView:  MaterialCalendarView
     private lateinit var countRecords: TextView
     private lateinit var workoutRoutine: TextView
+    private lateinit var partTextView: TextView
 
+    private val recordsViewModel = RecordsViewModel.get()
     private var calendarDays: MutableList<CalendarDay> = emptyList<CalendarDay>().toMutableList()
+    private lateinit var records: List<Record>
 
     @SuppressLint("SetTextI18n")
     override fun onCreateView(
@@ -38,7 +43,26 @@ class WorkoutCalendarFragment : Fragment() {
         calendarView = view.findViewById(R.id.calendarView)
         countRecords = view.findViewById(R.id.count_records)
         workoutRoutine = view.findViewById(R.id.routine_textview)
+        partTextView = view.findViewById(R.id.partTextView)
 
+        calendarView.setOnDateChangedListener(OnDateSelectedListener { _, date, _ ->
+            var tmp = ""
+            var selectedDay = (date.year).toString() + "년 " + (date.month+1).toString() + "월 " + (date.day).toString() + "일"
+            for (i in records.indices) {
+                if (CalendarDay(records[i].date) == date) {
+                    tmp = tmp + records[i].part + " "
+                }
+            }
+            if (tmp != "") {
+                partTextView.visibility = View.VISIBLE
+                workoutRoutine.visibility = View.VISIBLE
+                partTextView.text = getString(R.string.workout_part_calendar, selectedDay)
+                workoutRoutine.text = tmp
+            } else {
+                partTextView.visibility = View.INVISIBLE
+                workoutRoutine.visibility = View.INVISIBLE
+            }
+        })
         calendarView.addDecorators(
             NormalDayDecorator(),
             TodayDecorator(),
@@ -50,6 +74,22 @@ class WorkoutCalendarFragment : Fragment() {
         return view
     }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recordsViewModel.recordLiveData.observe(
+            viewLifecycleOwner,
+            { records ->
+                this.records = records
+                for (i in records.indices) {
+                    calendarDays.add(CalendarDay(records[i].date))
+                }
+                val count = records.size
+                countRecords.text = getString(R.string.record_count, count)
+            }
+        )
+    }
+
     override fun onResume() {
         super.onResume()
         //액션바에 텍스트 변경
@@ -57,6 +97,8 @@ class WorkoutCalendarFragment : Fragment() {
             (activity as WorkoutRecordActivity).setActionBarTitle(R.string.calendar)
         }
     }
+
+
 
     //오늘 날짜에 흰색 체크
     class TodayDecorator() : DayViewDecorator {
